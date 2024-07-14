d69002ea3c1127c75b5b4ee844576b115b4dc821
coldrain-f <intmain__@naver.com>
2024-07-14 18:44:03 +0900

    전체적인 구성요소 한글로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "./components/ui/card";
+} from "@/components/ui/card";
 import { Icons } from "@/components/icons/icons";
 import { SunIcon } from "@radix-ui/react-icons";
 import {
@@ -17,25 +17,36 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "./components/ui/carousel";
-import { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar";
+} from "@/components/ui/carousel";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/components/ui/use-toast";
 
 function App() {
+  const { toast } = useToast();
+
   return (
     <>
-      <div className="w-screen border h-12 shadow-sm sticky top-0 bg-white">
+      <div className="w-screen border h-12 shadow-sm bg-white sticky top-0">
         <div className="container mx-auto w-[800px] p-2">
           <div className="flex justify-between">
+            <a
+              href="#"
+              className="text-xl tracking-tight text-slate-700 hover:text-slate-500 font-semibold font-family-inter"
+            >
+              Home
+            </a>
+
             <div>
-              <a
-                href="#"
-                className="scroll-m-20 text-xl font-semibold tracking-tight text-slate-700"
+              <Button
+                variant="ghost"
+                size="sm"
+                onClick={() => {
+                  window.open("https://github.com/tasktrekTT");
+                }}
               >
-                Home
-              </a>
-            </div>
-            <div>
-              <Button variant="ghost" size="sm">
                 <Icons.gitHub className="h-4 w-4" />
               </Button>
               <Button variant="ghost" size="sm">
@@ -47,140 +58,256 @@ function App() {
       </div>
       <div className="container mx-auto w-[800px] p-4">
         <div>
-          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-stone-700 mt-6">
-            About me
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-stone-700 mt-1">
+            팀 소개
           </h3>
           <Separator className="my-4" />
 
-          <p className="leading-7 mt-6">
+          <p
+            className="leading-7 text-stone-700"
+            style={{ fontSize: "15px", lineHeight: "1.65rem" }}
+          >
             <img
               src="https://cdn.pixabay.com/photo/2024/05/02/09/16/web-development-8734249_1280.png"
-              className="w-48 h-32 float-left rounded-xl mr-5"
+              className="w-48 h-36 float-left rounded-xl mr-4 object-cover"
             />
-            Hello I'm{" "}
-            <a href="#" className="font-semibold underline text-stone-700">
-              coldrain
-            </a>{" "}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            eucondimentum ex. Quisque non augue tellus. Cras commodo elementum
-            vulputate.
+            안녕하세요! 저희는 다양한 프로젝트를 통해 CRUD, 검색 및 필터링, 웹
+            소켓 등 다양한 기능을 REST API로 구현하여{" "}
+            <span className="font-semibold">
+              프로그래밍 실력을 한 단계 더 향상
+            </span>
+            시키고자 합니다.
+            <div className="pt-4" />
+            또한, 백엔드 단의 테스트 코드 와 클린 코드 작성을 통해{" "}
+            <span className="font-semibold">
+              가독성이 높고 신뢰할 수 있는 코드 작성을 목표
+            </span>
+            로 계속해서 학습하고 리팩터링 해나갈 생각입니다.
           </p>
         </div>
 
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-stone-700 mt-20">
-          Projects
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-stone-700 mt-14">
+          프로젝트
         </h3>
         <p className="text-sm text-muted-foreground mt-1.5 mb-5">
-          Tap the card to go to the deployed project's website.
+          프로젝트의 Github 저장소로 이동할 수 있습니다.
         </p>
         <Separator className="my-4" />
+        <Tabs defaultValue="coldrain-f">
+          <TabsList>
+            <TabsTrigger value="coldrain-f">coldrain-f</TabsTrigger>
+            <TabsTrigger value="ksinny">ksinny</TabsTrigger>
+          </TabsList>
+          <TabsContent value="coldrain-f">
+            <Carousel>
+              <CarouselContent>
+                <CarouselItem>
+                  <div className="flex flex-wrap gap-4">
+                    <Card
+                      className="w-[376px] hover:bg-stone-50 cursor-pointer shadow-sm"
+                      onClick={() => {
+                        toast({
+                          title: "coldrain-f > Tasktrek Todo's",
+                          description:
+                            "Github 저장소가 아직 생성되지 않았습니다.",
+                          duration: 1000,
+                        });
+                      }}
+                    >
+                      <CardHeader>
+                        <CardTitle className="text-stone-700">
+                          Tasktrek Todo's
+                        </CardTitle>
+                        <CardDescription>
+                          할 일 관리 프로젝트로, 데이터베이스와 연동하여 CRUD,
+                          페이징, 검색 및 필터링 기능을 개발하였습니다.
+                        </CardDescription>
+                      </CardHeader>
 
-        <Carousel>
-          <CarouselContent>
-            <CarouselItem>
-              <div className="flex flex-wrap gap-4">
-                <Card className="w-[376px] hover:bg-stone-50 cursor-pointer shadow-sm">
-                  <CardHeader>
-                    <CardTitle>Tasktrek Todo's</CardTitle>
-                    <CardDescription>
-                      A project for managing tasks efficiently.
-                    </CardDescription>
-                  </CardHeader>
+                      <CardContent>
+                        <h3 className="scroll-m-20 text-base font-semibold tracking-tight text-slate-700 mb-2">
+                          Skills
+                        </h3>
+                        <div className="flex h-5 items-center space-x-3 text-sm">
+                          <div>Typescript</div>
+                          <Separator orientation="vertical" />
+                          <div>React</div>
+                          <Separator orientation="vertical" />
+                          <div>shadcn/ui</div>
+                        </div>
 
-                  <CardContent>
-                    <h3 className="scroll-m-20 text-base font-semibold tracking-tight text-stone-700 mb-2">
-                      Skills
-                    </h3>
-                    <div className="flex h-5 items-center space-x-4 text-sm">
-                      <div>React</div>
-                      <Separator orientation="vertical" />
-                      <div>Typescript</div>
-                      <Separator orientation="vertical" />
-                      <div>shadcn/ui</div>
-                    </div>
-                    <div className="flex h-5 items-center space-x-4 text-sm mt-2">
-                      <div>Spring Boot</div>
-                      <Separator orientation="vertical" />
-                      <div>Java</div>
-                      <Separator orientation="vertical" />
-                      <div>JPA</div>
-                    </div>
-                  </CardContent>
-                </Card>
+                        <div className="flex h-5 items-center space-x-3 text-sm mt-2">
+                          <div>Java</div>
+                          <Separator orientation="vertical" />
+                          <div>Spring</div>
+                          <Separator orientation="vertical" />
+                          <div>JPA</div>
+                          <Separator orientation="vertical" />
+                          <div>QueryDSL</div>
+                          <Separator orientation="vertical" />
+                          <div>MariaDB</div>
+                        </div>
+                      </CardContent>
+                    </Card>
 
-                <Card className="w-[376px] hover:bg-stone-50 cursor-pointer shadow-sm">
-                  <CardHeader>
-                    <CardTitle>Tasktrek Chat</CardTitle>
-                    <CardDescription>
-                      A server-side chat project utilizing web sockets.
-                    </CardDescription>
-                  </CardHeader>
+                    <Card
+                      className="w-[376px] hover:bg-stone-50 cursor-pointer shadow-sm"
+                      onClick={() => {
+                        toast({
+                          title: "coldrain-f > Tasktrek Chat",
+                          description:
+                            "Github 저장소가 아직 생성되지 않았습니다.",
+                          duration: 1000,
+                        });
+                      }}
+                    >
+                      <CardHeader>
+                        <CardTitle className="text-stone-700">
+                          Tasktrek Chat
+                        </CardTitle>
+                        <CardDescription>
+                          A server-side chat project utilizing web sockets.
+                        </CardDescription>
+                      </CardHeader>
 
-                  <CardContent>
-                    <h3 className="scroll-m-20 text-base font-semibold tracking-tight text-stone-700 mb-2">
-                      Skills
-                    </h3>
-                    <div className="flex h-5 items-center space-x-4 text-sm">
-                      <div>React</div>
-                      <Separator orientation="vertical" />
-                      <div>Typescript</div>
-                      <Separator orientation="vertical" />
-                      <div>shadcn/ui</div>
-                    </div>
-                    <div className="flex h-5 items-center space-x-4 text-sm mt-2">
-                      <div>Spring Boot</div>
-                      <Separator orientation="vertical" />
-                      <div>Java</div>
-                      <Separator orientation="vertical" />
-                      <div>JPA</div>
-                      <Separator orientation="vertical" />
-                      <div>Web Socket</div>
-                    </div>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
+                      <CardContent>
+                        <h3 className="scroll-m-20 text-base font-semibold tracking-tight text-slate-700 mb-2">
+                          Skills
+                        </h3>
+                        <div className="flex h-5 items-center space-x-4 text-sm">
+                          <div>React</div>
+                          <Separator orientation="vertical" />
+                          <div>Typescript</div>
+                          <Separator orientation="vertical" />
+                          <div>shadcn/ui</div>
+                        </div>
+                        <div className="flex h-5 items-center space-x-4 text-sm mt-2">
+                          <div>Spring Boot</div>
+                          <Separator orientation="vertical" />
+                          <div>Java</div>
+                          <Separator orientation="vertical" />
+                          <div>JPA</div>
+                          <Separator orientation="vertical" />
+                          <div>Web Socket</div>
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
 
-            <CarouselItem>
-              <div className="flex flex-wrap gap-4">
-                <Card className="w-[376px] hover:bg-stone-50 cursor-pointer shadow-sm">
-                  <CardHeader>
-                    <CardTitle>Tasktrek Calendar</CardTitle>
-                    <CardDescription>coming soon...</CardDescription>
-                  </CardHeader>
+                <CarouselItem>
+                  <div className="flex flex-wrap gap-4">
+                    <Card
+                      className="w-[376px] hover:bg-stone-50 cursor-pointer shadow-sm"
+                      onClick={() => {
+                        toast({
+                          title: "coldrain-f > Tasktrek Calendar",
+                          description:
+                            "Github 저장소가 아직 생성되지 않았습니다.",
+                          duration: 1000,
+                        });
+                      }}
+                    >
+                      <CardHeader>
+                        <CardTitle className="text-stone-700">
+                          Tasktrek Calendar
+                        </CardTitle>
+                        <CardDescription>coming soon...</CardDescription>
+                      </CardHeader>
 
-                  <CardContent>
-                    <h3 className="scroll-m-20 text-base font-semibold tracking-tight text-stone-700 mb-2">
-                      Skills
-                    </h3>
-                    <div className="flex h-5 items-center space-x-4 text-sm">
-                      <div>React</div>
-                      <Separator orientation="vertical" />
-                      <div>Typescript</div>
-                      <Separator orientation="vertical" />
-                      <div>shadcn/ui</div>
-                    </div>
-                    <div className="flex h-5 items-center space-x-4 text-sm mt-2">
-                      <div>Spring Boot</div>
-                      <Separator orientation="vertical" />
-                      <div>Java</div>
-                      <Separator orientation="vertical" />
-                      <div>JPA</div>
-                    </div>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+                      <CardContent>
+                        <h3 className="scroll-m-20 text-base font-semibold tracking-tight text-slate-700 mb-2">
+                          Skills
+                        </h3>
+                        <div className="flex h-5 items-center space-x-4 text-sm">
+                          <div>React</div>
+                          <Separator orientation="vertical" />
+                          <div>Typescript</div>
+                          <Separator orientation="vertical" />
+                          <div>shadcn/ui</div>
+                        </div>
+                        <div className="flex h-5 items-center space-x-4 text-sm mt-2">
+                          <div>Spring Boot</div>
+                          <Separator orientation="vertical" />
+                          <div>Java</div>
+                          <Separator orientation="vertical" />
+                          <div>JPA</div>
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </TabsContent>
+          <TabsContent value="ksinny">
+            <Carousel>
+              <CarouselContent>
+                <CarouselItem>
+                  <div className="flex flex-wrap gap-4">
+                    <Card
+                      className="w-[376px] hover:bg-stone-50 cursor-pointer shadow-sm"
+                      onClick={() => {
+                        toast({
+                          title: "ksinny > Tasktrek Todo's",
+                          description:
+                            "Github 저장소가 아직 생성되지 않았습니다.",
+                          duration: 1000,
+                        });
+                      }}
+                    >
+                      <CardHeader>
+                        <CardTitle className="text-stone-700">
+                          Tasktrek Todo's
+                        </CardTitle>
+                        <CardDescription>
+                          할 일 관리 프로젝트로, 데이터베이스와 연동하여 CRUD,
+                          페이징, 검색 및 필터링 기능을 개발하였습니다.
+                        </CardDescription>
+                      </CardHeader>
 
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-stone-700 mt-14">
-          Team
+                      <CardContent>
+                        <h3 className="scroll-m-20 text-base font-semibold tracking-tight text-stone-700 mb-2">
+                          Skills
+                        </h3>
+
+                        <div className="flex h-5 items-center space-x-3 text-sm">
+                          <div>Typescript</div>
+                          <Separator orientation="vertical" />
+                          <div>React</div>
+                          <Separator orientation="vertical" />
+                          <div>shadcn/ui</div>
+                        </div>
+
+                        <div className="flex h-5 items-center space-x-3 text-sm mt-2">
+                          <div>Java</div>
+                          <Separator orientation="vertical" />
+                          <div>Spring</div>
+                          <Separator orientation="vertical" />
+                          <div>JPA</div>
+                          <Separator orientation="vertical" />
+                          <div>QueryDSL</div>
+                          <Separator orientation="vertical" />
+                          <div>MariaDB</div>
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </TabsContent>
+        </Tabs>
+
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-stone-700 mt-6">
+          개발자
         </h3>
         <p className="text-sm text-muted-foreground mt-1.5 mb-5">
-          Tap the card to go to the team member's Github.
+          프로젝트에 참여한 개발자의 Github 링크로 이동할 수 있습니다.
         </p>
 
         <Separator className="my-4" />
@@ -194,7 +321,7 @@ function App() {
             <p className="mb-3 font-semibold">coldrain-f</p>
             <div className="flex">
               <Avatar className="mr-2 mt-[1px]">
-                <AvatarImage src="https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/9484def5-5edf-41b6-93dc-97a09580596e/width=1125/9484def5-5edf-41b6-93dc-97a09580596e.jpeg" />
+                <AvatarImage src="https://ui.shadcn.com/avatars/02.png" />
                 <AvatarFallback>SW</AvatarFallback>
               </Avatar>
               <div>
@@ -219,7 +346,7 @@ function App() {
             </div>
             <div className="flex">
               <Avatar className="mr-2 mt-[1px]">
-                <AvatarImage src="https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/15eec1aa-1584-45c5-8c2d-2f3b54e469cf/width=1125/15eec1aa-1584-45c5-8c2d-2f3b54e469cf.jpeg" />
+                <AvatarImage src="https://ui.shadcn.com/avatars/01.png" />
                 <AvatarFallback>KS</AvatarFallback>
               </Avatar>
               <div>
@@ -234,6 +361,7 @@ function App() {
           </div>
         </div>
       </div>
+      <Toaster />
     </>
   );
 }
